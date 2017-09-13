aa1c74388ad3a32d14efef09e4c1e55adc8fa702
Muhammad Azeez <Muhammad-Yasin@outlook.com>
2017-09-13 09:47:01 +0300

    Add project files.

---
+++ Module1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Module1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
